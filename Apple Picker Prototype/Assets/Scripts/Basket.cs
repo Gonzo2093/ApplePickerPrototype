1d03f167c5fdb763f4a68a8731f347cddd211cac
Babchin <babchin@list.ru>
2022-09-03 19:02:30 +0300

    Added UI. Modified script "Basket"

--- Apple Picker Prototype/Assets/Scripts/Basket.cs
+++ Apple Picker Prototype/Assets/Scripts/Basket.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    void Start()
+    {
+        // Получить ссылку на игровой объект ScoreCounter
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Получить компонент Text этого игрового объекта
+        scoreGT = scoreGO.GetComponent<Text>();
+        // Установить начальное число очков равным 0
+        scoreGT.text = "0";
+    }
     void Update()
     {
         // Получить текущие координаты указателя мыши на экране из Input
@@ -29,5 +42,12 @@ public class Basket : MonoBehaviour
         GameObject collidedWith = coll.gameObject;
         if (collidedWith.tag == "Apple")
             Destroy (collidedWith);
+
+        // Преобразовать текст в scoreGT в целое число
+        int score = int.Parse(scoreGT.text);
+        score += 1;
+
+        // Преобразовать число очков обратно в строку и вывести на экран
+        scoreGT.text = score.ToString();
     }
 }
