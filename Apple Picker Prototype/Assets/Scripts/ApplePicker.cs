1507e3f18efeae7dc8fff46365324acc6d771896
Babchin <babchin@list.ru>
2022-09-03 19:22:32 +0300

    Update ApplePicker.cs

--- Apple Picker Prototype/Assets/Scripts/ApplePicker.cs
+++ Apple Picker Prototype/Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,14 +10,17 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
     public void AppleDestroyed()
@@ -27,5 +31,14 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGO);
         }
+        // Удалить одну корзину. Получить индекс последней корзины
+        int basketIndex = basketList.Count - 1;
+
+        // Получить ссылку на этот игровой объект Basket
+        GameObject tBasketGO = basketList[basketIndex];
+
+        // Исключить корзину из списка и удалить сам игровой объект
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 }
