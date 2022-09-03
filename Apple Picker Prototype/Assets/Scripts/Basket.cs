7d5279dce5a22c5cfb00b396bdd2827d3b197ea8
Babchin <babchin@list.ru>
2022-09-03 18:31:50 +0300

    Add script "Basket"

--- Apple Picker Prototype/Assets/Scripts/Basket.cs
+++ Apple Picker Prototype/Assets/Scripts/Basket.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // Получить текущие координаты указателя мыши на экране из Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // Координаты Z камеры определяет, как далеко в трехмерном пространстве
+        // находится указатель мыши
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Преобразовать точку на двумерной плоскости экрана в трехмерные
+        // координаты игры
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Переместить корзину вдоль оси Х в координату Х указателя мыши
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        // Отыскать яблоко, попавшее в эту корзину
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+            Destroy (collidedWith);
     }
 }
